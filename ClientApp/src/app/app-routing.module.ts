f8b3af6163675657c0f45605e7db8fa597268002
Ale <alejandro@kzsoftworks.com>
2019-04-04 15:15:27 -0300

    i Finished the main page, and the routing from the main page (dashboard)

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { InformationComponent } from './information/information.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ProductsComponent } from './products/products.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'Dashboard', component: LandingComponent },
+  { path: 'Reports', component: ReportsComponent },
+  { path: 'Products', component: ProductsComponent },
+  { path: 'Employees', component: EmployeesComponent },
+  { path: 'Information', component: InformationComponent },
+  { path: '**', component: LandingComponent },
+  { path: '', component: LandingComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ProductsComponent } from './products/products.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { InformationComponent } from './information/information.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ReportsComponent,
+    ProductsComponent,
+    EmployeesComponent,
+    InformationComponent,
+    NavmenuComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
