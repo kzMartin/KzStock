eb73ab1447ae59e169d448379b9863ad4e95a9e4
Ale <alejandro@kzsoftworks.com>
2019-04-11 18:04:59 -0300

    Product table is in progress

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { InformationComponent } from './information/information.component';
+import { LandingComponent } from './landing/landing.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { ProductsComponent } from './products/products.component';
+import { ReportsComponent } from './reports/reports.component';
+import { AgGridModule } from 'ag-grid-angular';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { LandingComponent } from "./landing/landing.component";
-import { ReportsComponent } from "./reports/reports.component";
-import { ProductsComponent } from "./products/products.component";
-import { EmployeesComponent } from "./employees/employees.component";
-import { InformationComponent } from "./information/information.component";
-import { NavmenuComponent } from "./navmenu/navmenu.component";
 
 @NgModule({
   declarations: [
@@ -24,6 +26,7 @@ import { NavmenuComponent } from "./navmenu/navmenu.component";
     BrowserModule,
     AppRoutingModule,
     AngularFontAwesomeModule,
+    AgGridModule.withComponents([])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ClientApp/src/app/products/products.component.ts
+++ ClientApp/src/app/products/products.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.scss"]
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  columnDefs = [
+    { headerName: 'Make', field: 'make' },
+    { headerName: 'Model', field: 'model' },
+    { headerName: 'Price', field: 'price' }
+  ];
+
+  rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxter', price: 72000 }
+  ];
 }
