f0a3b65128bb8bd3bb3c03d6e31e5f2453e7f437
Ale <alejandro@kzsoftworks.com>
2019-04-16 15:09:08 -0300

    add table component, show the data from the api in the table.

--- ClientApp/src/app/products/products.component.ts
+++ ClientApp/src/app/products/products.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { Product } from './product';
 import { ProductService } from './product.service';
 
@@ -10,19 +11,41 @@ import { ProductService } from './product.service';
 export class ProductsComponent implements OnInit {
 
   products: Product[];
+  allProducts: Product[];
 
   columns = [
-    { prop: 'Id' },
-    { name: 'Name' },
-    { name: 'UnitPrice' },
-    { name: 'Stock' }
+    { name: 'id' },
+    { name: 'name' },
+    { name: 'stock' },
+    { name: 'unitPrice' }
   ];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService
-      .getProducts()
-      .subscribe(products => (this.products = products));
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.allProducts = products;
+    });
+  }
+
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    const result = this.allProducts.filter(product => {
+      return product.name.toLowerCase().includes(val);
+    });
+
+    if (val !== '') {
+      this.products = result;
+    } else {
+      this.products = this.allProducts;
+    }
+  }
+  edit(product: Product) {
+    alert('edit works');
+  }
+  delete(product: Product) {
+    alert('delete works');
   }
 }
