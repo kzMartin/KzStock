d2bfb7d41d248eeb67fe7484abb4f52175a62bba
Ale <alejandro@kzsoftworks.com>
2019-05-21 17:56:54 -0300

    list all employees and products in reports

--- ClientApp/src/app/reports/reports.component.ts
+++ ClientApp/src/app/reports/reports.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import { EmployeeService } from './../employees/employee.service';
+import { ProductService } from 'src/app/products/product.service';
+import { Product } from './../products/product';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employees/employee';
 
 @Component({
-  selector: "app-reports",
-  templateUrl: "./reports.component.html",
-  styleUrls: ["./reports.component.scss"]
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.scss']
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() {}
+  employees: Employee[];
+  products: Product[];
+
+  columns = [
+    { name: 'name' },
+  ];
+
+  constructor(private productService: ProductService, private employeeService: EmployeeService) {}
 
   ngOnInit() {
+    this.getProducts();
+    this.getEmployees();
   }
 
+  getProducts() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+    });
+  }
+
+  getEmployees() {
+    this.employeeService.getEmployees().subscribe(employees => {
+      this.employees = employees;
+    });
+  }
 }
